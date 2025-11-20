606872c867c77ce4f8a007fedd741bb5612d3a37
Modan Andrei <xmodan.andrei@gmail.com>
2025-11-20 15:24:18 +0200

    Update split_data.py

--- src/preprocessing/split_data.py
+++ src/preprocessing/split_data.py
@@ -1,1 +1,13 @@
+from sklearn.model_selection import train_test_split
+
+def split_dataset(images, labels):
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        images, labels, test_size=0.30, stratify=labels, random_state=42
+    )
+
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp, y_temp, test_size=0.50, stratify=y_temp, random_state=42
+    )
+
+    return (X_train, y_train), (X_val, y_val), (X_test, y_test)
 
