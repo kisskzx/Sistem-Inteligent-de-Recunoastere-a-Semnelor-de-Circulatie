3748c05ee9f2e6504069886aaf7ec432343cd943
Modan Andrei <xmodan.andrei@gmail.com>
2025-11-20 15:14:05 +0200

    Implement load_images_from_folder function
    Added function to load images from a specified folder, handling various image formats and corrupt files.

--- src/preprocessing/load_data.py
+++ src/preprocessing/load_data.py
@@ -1,1 +1,24 @@
+import os
+import cv2
 
+def load_images_from_folder(folder):
+    images = []
+    labels = []
+    paths = []
+
+    for root, _, files in os.walk(folder):
+        for f in files:
+            if f.lower().endswith((".ppm", ".png", ".jpg", ".jpeg")):
+                path = os.path.join(root, f)
+                img = cv2.imread(path)
+
+                if img is None:
+                    print("Imagine coruptă ignorată:", path)
+                    continue
+
+                label = os.path.basename(root)
+                labels.append(int(label))
+                images.append(img)
+                paths.append(path)
+
+    return images, labels, paths
