4dbdd7e620efb56b829a3867331d93d6cecc8252
Modan Andrei <xmodan.andrei@gmail.com>
2025-11-20 15:24:30 +0200

    Add function to clean images and labels

--- src/preprocessing/clean_data.py
+++ src/preprocessing/clean_data.py
@@ -1,1 +1,13 @@
+def clean_images(images, labels, paths):
+    clean_imgs = []
+    clean_lbls = []
+    clean_paths = []
+
+    for img, lbl, p in zip(images, labels, paths):
+        if img is not None and img.size > 0:
+            clean_imgs.append(img)
+            clean_lbls.append(lbl)
+            clean_paths.append(p)
+
+    return clean_imgs, clean_lbls, clean_paths
 
